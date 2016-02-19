8db82127b9923a06a2cd4af22ab93f301d918b26
Björn Palmqvist <bjorn@aidium.se>
2016-02-20 00:45:45 +0100

    Solution for Prime Factor Kata

--- java/tdd-training/src/test/java/PrimesTest.java
+++ java/tdd-training/src/test/java/PrimesTest.java
@@ -1,11 +1,23 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PrimesTest {
 
     @Test
-    public void prime_of_1() throws Exception {
-        int[] expected = new int[]{1};
-        Assert.assertArrayEquals(expected, Primes.of(1));
+    public void primes() throws Exception {
+        assertPrimes(Primes.of(1), 1);
+        assertPrimes(Primes.of(2), 2);
+        assertPrimes(Primes.of(4), 2, 2);
+        assertPrimes(Primes.of(6), 2, 3);
+        assertPrimes(Primes.of(8), 2, 2, 2);
+        assertPrimes(Primes.of(9), 3, 3);
+        assertPrimes(Primes.of(2 * 3 * 5 * 7 * 11 * 97), 2, 3, 5, 7, 11, 97);
+    }
+
+    private void assertPrimes(List<Integer> list, Integer... expected) {
+        Assert.assertEquals(Arrays.asList(expected), list);
     }
 }
