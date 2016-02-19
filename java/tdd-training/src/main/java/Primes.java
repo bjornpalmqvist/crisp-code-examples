2f14ef47d5cd5bed6ad9599229ad7b28c85c15a4
Björn Palmqvist <bjorn@aidium.se>
2016-02-20 00:45:45 +0100

    Solution for Prime Factor Kata

--- java/tdd-training/src/main/java/Primes.java
+++ java/tdd-training/src/main/java/Primes.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Primes {
 
 
-    public static int[] of(int i) {
-        return new int[]{1};
+    public static List<Integer> of(int i) {
+        if (i == 1) return Arrays.asList(1);
+
+        List<Integer> integers = new ArrayList<Integer>();
+        for (int divisor = 2; divisor <= i; ++divisor) {
+            for (; i % divisor == 0; i /= divisor) {
+                integers.add(divisor);
+            }
+        }
+        return integers;
     }
 }
